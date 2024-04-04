0efc705d29f7c2cbe7469673d1c144357e42f9e8
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-04-04 10:42:28 +0530

    Add files via upload

---
+++ MVC_Action_method/Controllers/Home_DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Action_method.Controllers
+{
+    public class Home_DefaultController : Controller //first get reqvest get  controlar
+    {
+        // GET: Home_Default
+        public ActionResult Index() //view generate karte he action mathods
+        {
+            return View("View_index");
+        }
+    }
+}
---
+++ MVC_Action_method/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Action_method.MvcApplication" Language="C#" %>
