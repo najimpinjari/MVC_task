0057ea4f2a17f1a075d7aa8731ebd74c34b06170
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-04-24 15:36:40 +0530

    Add files via upload

---
+++ Areas/App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Area_newone
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //routes.MapMvcAttributeRoutes(); // to enable attribute based routing 
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string []  { "Area_newone.Controllers" }
+            );
+        }
+    }
+}
