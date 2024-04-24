f4515a1a5c4c082dd4b9d760d9bfd35336505b9f
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-04-24 15:38:04 +0530

    Add files via upload

---
+++ Areas/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Area_newone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Area_newone.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel user)
+        {
+            if(user.Username == "admin" &&  user.Password == "admin")
+            {
+                return RedirectToAction("Index", "Home" , new {  area = "admin"});
+            }
+            else if (user.Username == "seller" && user.Password == "seller")
+            {
+                return RedirectToAction("Index", "Home", new { area = "seller" });
+
+            }
+            else if (user.Username == "user" && user.Password == "user")
+            {
+                return RedirectToAction("Index", "Home", new { area = "user" });
+
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home", new { area = "" });
+
+            }
+            return View();
+        }
+    }
+}
---
+++ Areas/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4_AreaAndFilters.Controllers
+{
+    // [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        // [RequireHttps]
+        // [HandleError]
+        public ActionResult Index()
+        {
+            //try
+            //{
+            //    throw new Exception("My Custom Exception Intentionally Thrown");                
+            //}
+            //catch
+            //{
+            //    return View("About");
+            //}
+
+            throw new Exception("My Custom Exception Intentionally Thrown");
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult PostComments(string comment)
+        {
+            ViewBag.Comment = comment;
+
+            return View("PostComments");
+        }
+
+        // [OutputCache(Duration = 10)]
+        [OutputCache(CacheProfile = "10SecsCache")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            ViewBag.LoadedTime = DateTime.Now;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [ChildActionOnly] // filter
+        public PartialViewResult LoginSection()
+        {
+            return PartialView("_LoginPartial");
+        }
+    }
+}
