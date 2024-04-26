9dd48e6a0d6d55de1701b6a1b83fa832ca16f838
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-04-26 18:50:05 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using View_bag_task.Models;
+
+namespace View_bag_task.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Message = "i am a full stack developer ";
+            ViewBag.Currentdate = DateTime.Now.ToLongDateString();
+
+            string[] friends = { "arbaj", "nilesh", "rupesh", "gotya" };
+            ViewBag.myfriends = friends;
+
+            ViewBag.sportlist = new List<string>()
+            {
+                "cricket",
+                    "hollyball",
+                    "football",
+                    "rummy"
+            };
+
+            Employee  employee = new Employee();
+
+            employee.Empdesignation = "maneger";
+            employee.EmpName = "rupesh";
+            employee.EmpId = 1;
+
+            ViewBag.Myemployee = employee;
+
+            ViewBag.commonmessagee = "this message is accessible for both the types view bag and view data  ";
+
+
+
+            return View();
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace View_bag_task.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Empdesignation { get; set; }
+    }
+}
