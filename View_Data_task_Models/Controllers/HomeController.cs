6efd4ccd44733854e3f096343348fcd371c116a6
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-04-26 17:58:28 +0530

    Add files via upload

---
+++ View_Data_task_Models/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using View_Data_task.Models;
+
+namespace View_Data_task.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Message from View Data !! ";
+            ViewData["Currnettime"] = DateTime.Now.ToLongTimeString();
+
+            string[] friends = { "rupesh", "arbaj", "nilesh", "gotya" };
+            ViewData["myfriends"] = friends;
+
+            ViewData["sportlist"] = new List<string>
+            {
+                 "cricket",
+                "football",
+                "hockey",
+                "volleyball"
+            };
+
+            Employee arbaj = new Employee();
+            arbaj.Empid = 11;
+            arbaj.EmpName = "Arbaj";
+            arbaj.EmpDesigrantion = "JrEnginner";
+
+            ViewData["Employee"] = arbaj;
+               
+               
+
+            return View();
+        }
+    }
+}
---
+++ View_Data_task_Models/View_Data_task_Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace View_Data_task.Models
+{
+    public class Employee
+    {
+        public int Empid { get; set; }
+        public string EmpName { get; set; }
+        public string EmpDesigrantion { get; set; }
+
+    }
+}
