0d561063e4918801285e3cd7ea020c8d93644c70
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-04-27 10:28:46 +0530

    Add files via upload

---
+++ Session_task/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Session_task.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Var1"] = "data comes from viewdata";
+            ViewBag.Var2 = "data comes from viewbag";
+            TempData["Var3"] = "data comes from Tempdata";
+
+
+            Session["Var4"] = "data comes from sesion ";
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
